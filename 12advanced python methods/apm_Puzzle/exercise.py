d5058cc148ced79d24a3635f302846c3fe38583a
Pedro Pla <ppla2001@gmail.com>
2021-01-24 21:35:38 -0300

    arranque con puzzle de advanced modules

---
+++ 12advanced python methods/apm_Puzzle/exercise.py
@@ -0,0 +1,5 @@
+#primero vamos a unzip
+import shutil
+#le tengo que dar el directory de donde esta el archivo porq sino no lo encuentra
+shutil.unpack_archive('C:\\Users\\plape\\OneDrive\\Escritorio\\Python\\Python-Bootcamp-notas-ejercicios\\12advanced python methods\\apm_Puzzle\\unzip_me_for_instructions.zip','puzzle','zip')
+
