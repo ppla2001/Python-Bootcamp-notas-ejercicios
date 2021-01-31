1c47077e07d6b995120a3026a63caa4fa7172e89
Pedro Pla <ppla2001@gmail.com>
2021-01-31 16:00:58 -0300

    arranque advanced objects

---
+++ 17advanced Python Objects and Data Structures/01advancedNumbers.py
@@ -0,0 +1,16 @@
+#hexadecimal numbers 
+hex(12) #converts numbers into hexadecimal number representation
+hex(512)
+#binary representation
+bin(128)
+bin(512)
+#power of numbers
+pow(2,3) #con dos numeros es x**y
+pow(2,3,4) #tres numeros es x**y %z
+#absolute value function
+abs(-3)
+abs(2)
+#round number 
+round(3.1)
+round(3.9)
+round(3.141492,2) #el segundo numero es cuanto quiero que me devuelva
